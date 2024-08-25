5d78af511faffca13f88c2db6cbec4b02547a96f
LusmicSam <138333123+LusmicSam@users.noreply.github.com>
2024-08-25 15:42:27 +0530

    Add files via upload

---
+++ Day-05Questions.cpp
@@ -0,0 +1,50 @@
+//Question 1 
+//#include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Enter a number";
+//     int num;
+//     cin>>num;
+//     if(num&1){
+//         cout<<"it is ODD";
+//     }else{
+//         cout<<"it is EVEN";
+//     }
+// }
+
+//Question 2
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+//     string a;
+//     cout<<"Enter the string: ";
+//     getline(cin,a);
+//     reverse(a.begin(),a.end());
+//     cout<<"reversed string is: ";
+//     cout<<a;
+
+// }
+ 
+ //Question 3
+//  #include<iostream>
+//  using namespace std;
+//  void count_V_C(string str){
+//     int v=0,C=0;
+//     for(char ch:str){
+//         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+//             v++;
+//         }else{
+//             C++;
+//         }
+//     }
+//    cout<<"Number of vowels: "<<v<<endl;
+//    cout<<"Number of consonents: "<<C;
+//  }
+//  int main(){
+//     string str;
+//     cout<<"Enter the string: ";
+//     getline(cin, str);
+//     count_V_C(str);
+
+//  }
