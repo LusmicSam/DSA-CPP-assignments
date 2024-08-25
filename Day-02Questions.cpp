9d19aab6514454b88e06be6d8b0e81703e69da4f
LusmicSam <138333123+LusmicSam@users.noreply.github.com>
2024-08-25 14:23:35 +0530

    Add files via upload

---
+++ Day-02Questions.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+//Question 1
+// int main() {
+//     int num1, num2, num3;
+
+//     cout << "Enter the first number: ";
+//     cin >> num1;
+//     cout << "Enter the second number: ";
+//     cin >> num2;
+//     cout << "Enter the third number: ";
+//     cin >> num3;
+
+//     if (num1 >= num2 && num1 >= num3) {
+//         cout << "The largest number is: " << num1 << endl;
+//     } else if (num2 >= num1 && num2 >= num3) {
+//         cout << "The largest number is: " << num2 << endl;
+//     } else {
+//         cout << "The largest number is: " << num3 << endl;
+//     }
+// }
+//Question 2
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int score;
+
+//     cout << "Enter the score: ";
+//     cin >> score;
+
+//     if (score >= 90 && score <= 100) {
+//         cout << "Grade: A" << endl;
+//     } else if (score >= 80 && score <= 89) {
+//         cout << "Grade: B" << endl;
+//     } else if (score >= 70 && score <= 79) {
+//         cout << "Grade: C" << endl;
+//     } else if (score >= 60 && score <= 69) {
+//         cout << "Grade: D" << endl;
+//     } else if (score >= 0 && score <= 59) {
+//         cout << "Grade: F" << endl;
+//     } else {
+//         cout << "Invalid score." << endl;
+//     }
+
+   
+// }
+//Question 3
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int month;
+
+//     cout << "Enter a number: ";
+//     cin >> month;
+
+//     switch (month) {
+//         case 1:
+//             cout << "January" << endl;
+//             break;
+//         case 2:
+//             cout << "February" << endl;
+//             break;
+//         case 3:
+//             cout << "March" << endl;
+//             break;
+//         case 4:
+//             cout << "April" << endl;
+//             break;
+//         case 5:
+//             cout << "May" << endl;
+//             break;
+//         case 6:
+//             cout << "June" << endl;
+//             break;
+//         case 7:
+//             cout << "July" << endl;
+//             break;
+//         case 8:
+//             cout << "August" << endl;
+//             break;
+//         case 9:
+//             cout << "September" << endl;
+//             break;
+//         case 10:
+//             cout << "October" << endl;
+//             break;
+//         case 11:
+//             cout << "November" << endl;
+//             break;
+//         case 12:
+//             cout << "December" << endl;
+//             break;
+//         default:
+//             cout << "Invalid number." << endl;
+//     }
+
+   
+// }
+
