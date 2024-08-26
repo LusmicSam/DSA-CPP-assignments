700dfc5ae84280f6f37246a6e0558ae1fa413fa1
LusmicSam <138333123+LusmicSam@users.noreply.github.com>
2024-08-26 19:19:07 +0530

    Add files via upload

---
+++ Day-01Questions.cpp
@@ -0,0 +1,51 @@
+//Question 3
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int number;
+//     cout << "Enter a number: ";
+//     cin >> number;
+//     int square = number * number;
+//     cout << "The square of " << number << " is: " << square << endl;
+    
+// }
+
+//Question 2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int grade1, grade2, grade3, grade4, grade5;
+//     cout << "Enter grade 1: ";
+//     cin >> grade1;
+//     cout << "Enter grade 2: ";
+//     cin >> grade2;
+//     cout << "Enter grade 3: ";
+//     cin >> grade3;
+//     cout << "Enter grade 4: ";
+//     cin >> grade4;
+//     cout << "Enter grade 5: ";
+//     cin >> grade5;
+    
+//     float average = (grade1 + grade2 + grade3 + grade4 + grade5) / 5.0;
+//     cout << "The average grade is: " << average << endl;
+    
+// }
+
+//Question 3
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     string str1, str2, result;
+//     cout << "Enter the first string: ";
+//     getline(cin, str1);
+//     cout << "Enter the second string: ";
+//     getline(cin, str2);
+//     result = str1 + str2;
+//     cout << "The concatenated string is: " << result << endl;
+   
+// }
+
