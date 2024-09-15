c7ba39c1307636c368c7859376133a0c3a1b145c
LusmicSam <138333123+LusmicSam@users.noreply.github.com>
2024-09-16 00:16:16 +0530

    Update Day-11Questions.cpp

---
+++ Day-11Questions.cpp
@@ -0,0 +1,89 @@
+// Ques 1: You are given an array and target value. Your task is to find the target value that exists in the array. If it exists inside an array then print YES otherwise NO.
+
+// Sample Input :- Array -[2,4,6,7,8]
+// Target: 6
+// Output: YES
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={2,4,6,7,8};
+//     int target;
+//     cin>>target;
+//     int n=sizeof(arr)/sizeof(arr[0]);///yeh ktu ho raha hai linked list nahi ho raha .... koi bachao....
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target){
+//             cout<<"YES"<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<"NO"<<endl;
+//     return 0;
+//}
+
+// Ques 2: You are given an array and target value. You have to find the index of target value. If target doest exist inside the array then print -1.Assume indexing is 1 based.
+
+// Sample Input :- Array -[2,4,6,7,8]
+// Target: 6
+// Output: 3
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={2,4,6,7,8};
+//     int target;
+//     cin>>target;
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target){
+//             cout<<i+1<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<-1<<endl;
+//     return 0;
+//}
+//eise user input nam ki bhi kuch cheez hoti hai but i am lazy...// Ques 1: You are given an array and target value. Your task is to find the target value that exists in the array. If it exists inside an array then print YES otherwise NO.
+
+// Sample Input :- Array -[2,4,6,7,8]
+// Target: 6
+// Output: YES
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={2,4,6,7,8};
+//     int target;
+//     cin>>target;
+//     int n=sizeof(arr)/sizeof(arr[0]);///yeh ktu ho raha hai linked list nahi ho raha .... koi bachao....
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target){
+//             cout<<"YES"<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<"NO"<<endl;
+//     return 0;
+//}
+
+// Ques 2: You are given an array and target value. You have to find the index of target value. If target doest exist inside the array then print -1.Assume indexing is 1 based.
+
+// Sample Input :- Array -[2,4,6,7,8]
+// Target: 6
+// Output: 3
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={2,4,6,7,8};
+//     int target;
+//     cin>>target;
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target){
+//             cout<<i+1<<endl;
+//             return 0;
+//         }
+//     }
+//     cout<<-1<<endl;
+//     return 0;
+//}
+//eise user input nam ki bhi kuch cheez hoti hai but i am lazy...
